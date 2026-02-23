4df722dad8ff3256249407f31972380084668f86
tmiguel78 <tmiguel78@yahoo.es>
2026-02-23 15:06:53 +0100

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
 import './App.css'
+// import { set } from 'mongoose';
 
 function App() {
  //Aquí tu código
+
+ const [name, setName] = useState('Sofía');
+ const [newName, setNewName] = useState('');
+
+const changeName = (selectedName) => {
+    return setName(selectedName)
+}
+return(
+    <div>
+        <h2>Teacher's name: {name}</h2>
+        <ul>
+            <li key="Data" onClick={() => changeName('Data')}>Data</li>
+            <li key="Reyes" onClick={() => changeName('Reyes')}>Reyes</li>
+            <li key="Soraya" onClick={() => changeName('Soraya')}>Soraya</li>
+        </ul>
+    </div>
+)
+
 }
 
 export default App
